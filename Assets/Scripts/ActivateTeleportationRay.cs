369d61eff3ff1fde443a6d1af2cf3642f940fb32
Andrey Kopylkov <andreykopylkov@gmail.com>
2023-05-25 21:14:33 +0600

    Hide teleportation path

---
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    [SerializeField] private XRInteractorLineVisual _xrInteractorLineVisual;
+    [SerializeField] private InputActionProperty _activateInput;
+
+    private void Update()
+    {
+        _xrInteractorLineVisual.enabled = _activateInput.action.ReadValue<float>() > 0.1f;
+    }
+}

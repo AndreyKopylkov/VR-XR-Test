5c80e56d54b1714d817f278d8252bb757669b67e
Andrey Kopylkov <andreykopylkov@gmail.com>
2023-05-29 15:57:33 +0600

    RayInteractor disable on Grab + layers for interact

---
+++ Assets/Scripts/ActivateGrabRay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActivateGrabRay : MonoBehaviour
+{
+    [SerializeField] private XRInteractorLineVisual _xrInteractorLineVisual;
+    [SerializeField] private XRRayInteractor _xrRayInteractor;
+    [SerializeField] private XRDirectInteractor _xrDirectInteractor;
+    
+    private void Update()
+    {
+        if (_xrDirectInteractor.interactablesSelected.Count == 0)
+        {
+            _xrRayInteractor.enabled = true;
+            _xrInteractorLineVisual.enabled = true;
+        }
+        else
+        {
+            _xrRayInteractor.enabled = false;
+            _xrInteractorLineVisual.enabled = false;
+        }
+    }
+}
